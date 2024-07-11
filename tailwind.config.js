ad649673835477d3936c673ae372a3e99a55ea36
anishossain515 <amirul447700@gmail.com>
2024-07-11 23:08:58 +0600

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      borderRadius: {
+        'larger' : '150px',
+        'big' : '250px'
+      },
+    },
+  },
+  plugins: [],
+}
+
